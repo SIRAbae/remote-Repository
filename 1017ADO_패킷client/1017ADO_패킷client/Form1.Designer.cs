2d93f786696b7e55c8e9691710f4ddc92fe5c6a3
SIRAbae <56426302+SIRAbae@users.noreply.github.com>
2020-03-12 01:53:48 +0900

    ??

--- 1017ADO_패킷client/1017ADO_패킷client/Form1.Designer.cs
+++ 1017ADO_패킷client/1017ADO_패킷client/Form1.Designer.cs
@@ -61,7 +61,8 @@
             this.기능ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1288, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1127, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -72,27 +73,27 @@
             this.서버연결ToolStripMenuItem,
             this.서버연결해제ToolStripMenuItem});
             this.메ToolStripMenuItem.Name = "메ToolStripMenuItem";
-            this.메ToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.메ToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
             this.메ToolStripMenuItem.Text = "메 뉴";
             // 
             // 프로그램종료ToolStripMenuItem
             // 
             this.프로그램종료ToolStripMenuItem.Name = "프로그램종료ToolStripMenuItem";
-            this.프로그램종료ToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.프로그램종료ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.프로그램종료ToolStripMenuItem.Text = "프로그램 종료";
             this.프로그램종료ToolStripMenuItem.Click += new System.EventHandler(this.프로그램종료ToolStripMenuItem_Click);
             // 
             // 서버연결ToolStripMenuItem
             // 
             this.서버연결ToolStripMenuItem.Name = "서버연결ToolStripMenuItem";
-            this.서버연결ToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.서버연결ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.서버연결ToolStripMenuItem.Text = "서버 연결";
             this.서버연결ToolStripMenuItem.Click += new System.EventHandler(this.서버연결ToolStripMenuItem_Click);
             // 
             // 서버연결해제ToolStripMenuItem
             // 
             this.서버연결해제ToolStripMenuItem.Name = "서버연결해제ToolStripMenuItem";
-            this.서버연결해제ToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.서버연결해제ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.서버연결해제ToolStripMenuItem.Text = "서버 연결 해제";
             this.서버연결해제ToolStripMenuItem.Click += new System.EventHandler(this.서버연결해제ToolStripMenuItem_Click);
             // 
@@ -101,13 +102,13 @@
             this.기능ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.회원가입ToolStripMenuItem});
             this.기능ToolStripMenuItem.Name = "기능ToolStripMenuItem";
-            this.기능ToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.기능ToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
             this.기능ToolStripMenuItem.Text = "기 능";
             // 
             // 회원가입ToolStripMenuItem
             // 
             this.회원가입ToolStripMenuItem.Name = "회원가입ToolStripMenuItem";
-            this.회원가입ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.회원가입ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.회원가입ToolStripMenuItem.Text = "회원가입";
             this.회원가입ToolStripMenuItem.Click += new System.EventHandler(this.회원가입ToolStripMenuItem_Click);
             // 
@@ -116,9 +117,10 @@
             this.panel1.BackColor = System.Drawing.Color.Red;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1288, 60);
+            this.panel1.Size = new System.Drawing.Size(1127, 48);
             this.panel1.TabIndex = 2;
             // 
             // label1
@@ -128,7 +130,7 @@
             this.label1.ForeColor = System.Drawing.Color.Blue;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 15);
+            this.label1.Size = new System.Drawing.Size(121, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "서버를 연결해 주세요";
             // 
@@ -136,19 +138,21 @@
             // 
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(0, 88);
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 72);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(623, 649);
+            this.listBox1.Size = new System.Drawing.Size(546, 518);
             this.listBox1.TabIndex = 3;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(724, 252);
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(634, 202);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(497, 334);
+            this.listBox2.Size = new System.Drawing.Size(435, 268);
             this.listBox2.TabIndex = 4;
             // 
             // label2
@@ -156,17 +160,18 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(702, 114);
+            this.label2.Location = new System.Drawing.Point(614, 91);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.Size = new System.Drawing.Size(35, 16);
             this.label2.TabIndex = 7;
             this.label2.Text = " I D";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1095, 603);
+            this.button1.Location = new System.Drawing.Point(958, 482);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 41);
+            this.button1.Size = new System.Drawing.Size(83, 33);
             this.button1.TabIndex = 10;
             this.button1.Text = "전 송";
             this.button1.UseVisualStyleBackColor = true;
@@ -177,25 +182,27 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(702, 609);
+            this.label3.Location = new System.Drawing.Point(614, 487);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.Size = new System.Drawing.Size(59, 16);
             this.label3.TabIndex = 9;
             this.label3.Text = "입력창";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(786, 604);
+            this.textBox2.Location = new System.Drawing.Point(688, 483);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(276, 40);
+            this.textBox2.Size = new System.Drawing.Size(242, 33);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(786, 157);
+            this.textBox3.Location = new System.Drawing.Point(688, 126);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(361, 25);
+            this.textBox3.Size = new System.Drawing.Size(316, 21);
             this.textBox3.TabIndex = 6;
             // 
             // label4
@@ -203,17 +210,18 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(702, 162);
+            this.label4.Location = new System.Drawing.Point(614, 130);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.Size = new System.Drawing.Size(46, 16);
             this.label4.TabIndex = 7;
             this.label4.Text = " P W";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(771, 198);
+            this.button2.Location = new System.Drawing.Point(675, 158);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 41);
+            this.button2.Size = new System.Drawing.Size(83, 33);
             this.button2.TabIndex = 10;
             this.button2.Text = "로그인";
             this.button2.UseVisualStyleBackColor = true;
@@ -221,9 +229,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(897, 198);
+            this.button3.Location = new System.Drawing.Point(785, 158);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 41);
+            this.button3.Size = new System.Drawing.Size(83, 33);
             this.button3.TabIndex = 10;
             this.button3.Text = "로그아웃";
             this.button3.UseVisualStyleBackColor = true;
@@ -231,9 +240,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(1027, 198);
+            this.button4.Location = new System.Drawing.Point(899, 158);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 41);
+            this.button4.Size = new System.Drawing.Size(83, 33);
             this.button4.TabIndex = 10;
             this.button4.Text = "회원탈퇴";
             this.button4.UseVisualStyleBackColor = true;
@@ -241,17 +251,18 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(786, 109);
+            this.textBox1.Location = new System.Drawing.Point(688, 87);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(361, 25);
+            this.textBox1.Size = new System.Drawing.Size(316, 21);
             this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1288, 737);
+            this.ClientSize = new System.Drawing.Size(1127, 590);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -267,6 +278,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
